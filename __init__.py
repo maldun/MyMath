9a4bcc96d46eae1521d9e2844d2c6946659fbe33
Stefan Reiterer <stefan.reiterer@magnasteyr.com>
2015-02-06 09:36:21 +0100

    added __init__.py

---
+++ __init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# MyMath Module - API for easier Salome geompy usage
+# __init__.py
+#
+# Copyright (C) 2015  Stefan Reiterer - stefan.harald.reiterer@gmail.com
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
